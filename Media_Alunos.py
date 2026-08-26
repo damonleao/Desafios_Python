569cfc6cc8fb23557af25786ad998bd3c60c4e11
Damon Leão <damonleaodl@gmail.com>
2026-08-26 19:56:54 -0300

    Add student grade calculation and approval logic

---
+++ Media_Alunos.py
@@ -0,0 +1,20 @@
+while True:
+
+    n1 = float(input("Digite a nota 1: "))
+    n2 = float(input("Digite a nota 2: "))
+    n3 = float(input("Digite a nota 3: "))
+    n4 = float(input("Digite a nota 4: "))
+
+    media = (n1 + n2 + n3 + n4) / 4
+
+    print("Média:", media)
+
+    if media >= 6:
+        print("ALUNO APROVADO! PARABÉNS!!!")
+    else:
+        print("ALUNO REPROVADO! ESTUDE MAIS!")
+
+    continuar = input("Deseja continuar? (s/n): ")
+
+    if continuar == "n":
+        break
